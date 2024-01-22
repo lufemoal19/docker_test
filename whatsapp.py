f1196bdd1b2965644032fb4470905e3ace9d76ff
lufemoal19 <lufemoal19@gmail.com>
2024-01-22 07:23:06 +0000

    fix whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -38,26 +38,6 @@ def draw_heart(emoji) -> str:
     # copy_to_clipboard(heart)
     return heart
 
-def copy_to_clipboard(text):
-    try:
-        subprocess.run(['xsel', '-i'], input = text.encode(), check=True)
-    except FileNotFoundError:
-        print("xsel command not found. Clipboard operations may not work.")
-
-def paste_text():
-    try:
-        result = subprocess.run(['xsel', '-o'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-        if result.returncode == 0:
-            return result.stdout.strip()
-        else:
-            print("Error: Unable to paste text using xsel.")
-            return None
-    except FileNotFoundError:
-        print("Error: xsel command not found. Make sure xsel is installed.")
-        return None
-
-
 def send_message(messages):
     cellphone = data.loc[0, 'cellphone'].astype(str) # cast to string
     whatsapp_path = f'https://web.whatsapp.com/send?phone={cellphone}'
